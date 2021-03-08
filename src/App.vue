f35b4ac63230a3b99572061b29908a3b7cb255b2
Tolu Alonge <tolualonge123@gmail.com>
2021-03-08 21:11:39 +0100

    first bistel food app

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home msg="Welcome to Your Vue.js App"/>
+
+    ssss
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+   
+
+</style>
