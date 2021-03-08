7db915c09651416b003d1dcf5fd5cb45b6ae2e63
Tolu Alonge <tolualonge123@gmail.com>
2021-03-08 21:48:10 +0100

    first push to netlify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -78,7 +78,7 @@
         <!-- banner part ends -->
         <!-- location match part starts -->
         <div class="location-match text-xs-center">
-            <div class="container"> <span>356 restaurants matched your location: <span class="primary-color">New York, NY -</span> </span>
+            <div class="container"> <span>356 restaurants matched your location: <span class="primary-color">lagos, Nigeria -</span> </span>
             </div>
         </div>
         <!-- location match part ends -->
